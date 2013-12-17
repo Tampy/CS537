87719d602503fefbc94b24d61e7ab47062b20bda
Tampy <tpmx13@gmail.com>
2013-12-17 12:16:26 -0500

    Extraneous Files
    Removed some files that don't need to be in the project.

--- Final/skyboxWindows/shaders/skyboxfragment.glsl
+++
@@ -1,9 +0,0 @@
-#version 150
-in  vec3 texCoord;
-uniform samplerCube cubeMap;
-void main() {
-   
-   gl_FragColor = texture(cubeMap, texCoord);
-   gl_FragColor.a = 1.0;
-   
-}
--- Final/skyboxWindows/shaders/vshader30_TwoCubes_FullPipe.glsl
+++
@@ -1,9 +0,0 @@
-#version 150
-
-in  vec4 vPosition;
-uniform mat4 projection;
-uniform mat4 modelview;
-void main() 
-{ 
-  gl_Position = projection*modelview*vPosition;
-}
